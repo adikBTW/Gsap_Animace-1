7d3a1a316084cbee632e5642cc7391e2d0833dc4
adikBTW <114757477+adikBTW@users.noreply.github.com>
2024-10-04 18:22:58 +0200

    Add files via upload

---
+++ portfolio 1/script.js
@@ -0,0 +1,36 @@
+gsap.from(".první-text", {
+    opacity: 0, 
+    x: 300, 
+    duration: 1
+  });
+
+  gsap.from(".container", {
+    opacity: 0, 
+    x: 300, 
+    duration: 1
+  });
+
+  gsap.from(".kontakty", {
+    opacity: 0, 
+    y: 300, 
+    duration: 1
+});
+
+  gsap.from(".soc", {
+    rotation: -360, 
+    x: -100, 
+    duration: 1
+});
+
+gsap.from(".container-portfolio", {
+    opacity: 0, 
+    y: 300, 
+    duration: 1
+});
+
+
+gsap.from(".slideshow-container", {
+    opacity: 0, 
+    y: -300, 
+    duration: 1
+});
